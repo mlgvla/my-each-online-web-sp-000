3858163943805dfef2a8128bc5d0d7161ef79dbf
Monica Gerard <mlgerardvla@gmail.com>
2020-03-13 18:16:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+words = []
+
+def my_each(array)
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+end
+
+my_each(words) do |word|
+
 end
